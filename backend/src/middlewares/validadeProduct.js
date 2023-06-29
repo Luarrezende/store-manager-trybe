71f7c13bd634c289abe10fe00c7afbc39a95472b
Luar <luarrezende@gmail.com>
2023-06-29 14:38:54 -0300

    feat: resolve requisito 4 sem teste

---
+++ backend/src/middlewares/validadeProduct.js
@@ -0,0 +1,15 @@
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  const nameRegex = name < 5;
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+  if (!nameRegex) {
+    return res.status(422).json({ message: '"name" length must be at least 5 characters long' });
+  }
+  next();
+};
+
+module.exports = {
+  validateName,
+};
--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -3,11 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const { productController } = require('../controllers');
+const { validateName } = require('../middlewares/validadeProduct');
 
 router.get('/', productController.getAll);
 
 router.get('/:id', productController.getId);
 
-router.post('/', productController.postName);
+router.post('/', validateName, productController.postName);
 
 module.exports = router;
