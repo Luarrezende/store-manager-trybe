a0a99834ff5b52298f3e672d7934f6966bd5bc42
Luar <luarrezende@gmail.com>
2023-07-03 11:18:02 -0300

    fix: testes da 4

--- backend/tests/unit/middlewares/product.test.js
+++ backend/tests/unit/middlewares/product.test.js
@@ -1,45 +1,80 @@
-// const sinon = require('sinon');
-// const { expect } = require('chai');
-// const { validateName } = require('../../../src/middlewares/validadeProduct');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const sinon = require('sinon');
+const { expect } = require('chai');
 
-// describe('verifica middlewares', function () {
-//   it('deve passar para o próximo middleware quando o campo name é válido', async function () {
-//     const next = sinon.stub();
-//     const req = { body: { name: 'John' } };
-//     const res = {
-//       status: sinon.stub().returnsThis(),
-//       json: sinon.stub(),
-//     };
+chai.use(sinonChai);
 
-//     await validateName(req, res, next);
+const { validateName, minNameLength } = require('../../../src/middlewares/validadeProduct');
 
-//     expect(res.status).to.not.have.been.called();
-//     expect(res.json).to.not.have.been.called();
-//     expect(next).to.have.been.called();
-//   });
-//   it('deve retornar erro 400 quando o campo name está ausente', function () {
-//     const req = { body: { name: '' } };
-//     const res = { status: sinon.stub(), json: sinon.stub() };
-//     const next = sinon.stub();
+describe('verifica de products', function () {
+  it('deve passar para o próximo middleware quando o campo name é válido', async function () {
+    const req = {};
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    };
 
-//     validateName(req, res, next);
+    const next = sinon.stub();
+    
+    req.body = { name: 'product' };
+    
+    validateName(req, res, next);
 
-//     expect(res.status).to.have.been.calledWith(400);
-//     expect(res.json).to.have.been.calledWith({ message: '"name" is required' });
-//     expect(next).to.not.have.been.called();
-//   });
-//   it('deve retornar erro 422 quando o campo name tem menos de 5 caracteres', function () {
-//     const req = { body: { name: 'Bob' } };
-//     const res = { status: sinon.stub(), json: sinon.stub() };
-//     const next = sinon.stub();
+    expect(next.called).to.be.equal(true);
+    expect(res.status.called).to.be.equal(false);
+    expect(res.json.called).to.be.equal(false);
+  });
+  it('deve retornar erro 400 quando o campo name está ausente', function () {
+    const req = {};
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    };
 
-//     validateName(req, res, next);
+    const next = sinon.stub();
+    
+    req.body = {};
+    
+    validateName(req, res, next);
 
-//     expect(res.status).to.have.been.calledWith(422);
-//     expect(res.json).to.have.been.calledWith({ message: '"name" length must be at least 5 characters long' });
-//     expect(next).to.not.have.been.called();
-//   });
-//   afterEach(function () {
-//     sinon.restore();
-//   });
-// });
+    expect(next.called).to.be.equal(false);
+    expect(res.status.calledWith(400)).to.be.equal(true);
+    expect(res.json.calledWith({ message: '"name" is required' })).to.be.equal(true);
+  });
+  it('deve retornar erro 422 quando o campo name tem menos de 5 caracteres', function () {
+    const req = {};
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    };
+
+    const next = sinon.stub();
+    
+    req.body = { name: 'caix' };
+    
+    minNameLength(req, res, next);
+
+    expect(next.called).to.be.equal(false);
+    expect(res.status.calledWith(422)).to.be.equal(true);
+    expect(res.json.calledWith({ message: '"name" length must be at least 5 characters long' })).to.be.equal(true);
+  });
+  it('verifica se o next é chamado na função minNameLength', function () {
+    const req = {};
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.spy(),
+    };
+
+    const next = sinon.stub();
+    
+    req.body = { name: 'caixa' };
+    
+    minNameLength(req, res, next);
+
+    expect(next.called).to.be.equal(true);
+  });
+  afterEach(function () {
+    sinon.restore();
+  });
+});
